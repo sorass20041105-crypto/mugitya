507d6cd17d032b817dad96bf4dbe51a0d0ba6128
sorass20041105-crypto <sorass20041105@gmail.com>
2025-11-20 16:21:44 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title ("こんにちは、吉村ゼミ")
+
+st.text_input("好きな言葉を入力してください")
+
+st.write(name)
+
+camera_phote = st.camera_input("写真を撮影します")
+if camera:
+    st.image(camera, caption="写真", use_column_width=true)
