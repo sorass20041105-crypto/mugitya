64fb43d2f90631c5b6b9c5e4a93dcb5cf2341f23
sorass20041105-crypto <sorass20041105@gmail.com>
2025-11-27 15:20:38 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,6 +6,8 @@ name = st.text_input("好きな言葉を入力してください")
 
 st.write(name)
 
+st.checkbox("同意します")
+
 camera_phote = st.camera_input("写真を撮影します")
 if camera:
     st.image(camera, caption="写真", use_column_width=true)
