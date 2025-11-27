42df154f3a73fddaedac421f601608bfc9567693
sorass20041105-crypto <sorass20041105@gmail.com>
2025-11-27 15:37:50 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,7 +13,9 @@ st.write(address)
 hobby = st.multiselect("次の中から複数選択してください",["映画","音楽","散歩"])
 st.write(hobby)
 
-st.slider("この映画を10点満点で評価してください",0,10,5)
+score = st.slider("この映画を10点満点で評価してください",0,10,5)
+st.write(score)
+
 
 camera_phote = st.camera_input("写真を撮影します")
 if camera:
