8f9f8d7e8e7dc31e1e24d0d2f927fabe9c00a699
sorass20041105-crypto <sorass20041105@gmail.com>
2025-11-27 15:24:21 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,7 +7,7 @@ name = st.text_input("好きな言葉を入力してください")
 st.write(name)
 
 st.checkbox("同意します")
-st.selectbox("次の中から現住所を教えてください",["京都府""大阪"])
+st.selectbox("次の中から現住所を教えてください",["京都府","大阪"])
 
 camera_phote = st.camera_input("写真を撮影します")
 if camera:
